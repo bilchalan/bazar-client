c01993aa2862bdc9b8eada224e123dbb04f8b431
Monir U Zaman <bilchalan@gmail.com>
2023-06-09 23:25:23 +0600

    api changed

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000",
+  //baseURL: "http://localhost:4000",
+  baseURL: "https://bazar-api.onrender.com",
   withCredentials: true,
   headers: { "Content-Type": "application/form-data" },
 });
